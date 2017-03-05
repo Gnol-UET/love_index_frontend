08ab734acc47de88f3378fc18507437e0e4f067b
Long <zzsushiboyzz@gmail.com>
2017-03-05 13:09:25 +0700

    thay doi cach nhap ngay

--- modules/love/loveCtrl.js
+++ modules/love/loveCtrl.js
@@ -2,6 +2,8 @@ angular.module('loveModule')
     .controller('loveController', function ($scope, loveService, $http, $httpParamSerializerJQLike) {
         $scope.comment = '';//Xử lý logic ở đây
         $scope.score='';
+        $scope.date0 = '';
+        $scope.date1 = '';
         $scope.user0 = {
             name : "",
             day : 15,
@@ -14,8 +16,32 @@ angular.module('loveModule')
             month: 10,
             year : 97
         };
+        var getMonth = function(monthStr){
+            return new Date(monthStr+'-1-01').getMonth()+1
+        };
 
         $scope.calc = function () {
+            $scope.user0.month = getMonth($scope.date0.toDateString().substring(4,7));
+            $scope.user0.day = parseInt($scope.date0.toDateString().substring(8,10));
+            if(parseInt($scope.date0.toDateString().substring(11,15)) <2000 ){
+                $scope.user0.year = parseInt($scope.date0.toDateString().substring(13,15));
+            }
+            if(parseInt($scope.date0.toDateString().substring(11,15)) >=2000 ){
+                $scope.user0.year = parseInt($scope.date0.toDateString().substring(13,15));
+                $scope.user0.year = $scope.user0.year + 100;
+            }
+
+
+
+            $scope.user1.month = getMonth($scope.date1.toDateString().substring(4,7));
+            $scope.user1.day = parseInt($scope.date1.toDateString().substring(8,10));
+            if(parseInt($scope.date1.toDateString().substring(11,15)) <2000 ){
+                $scope.user1.year = parseInt($scope.date1.toDateString().substring(13,15));
+            }
+            if(parseInt($scope.date1.toDateString().substring(11,15)) >=2000 ){
+                $scope.user1.year = parseInt($scope.date1.toDateString().substring(13,15));
+                $scope.user1.year = $scope.user1.year + 100;
+            }
             var request = {
                 "rapide_prenom[0]" : $scope.user0.name,
                 "rapide_prenom[1]" : $scope.user1.name,
@@ -25,6 +51,7 @@ angular.module('loveModule')
                 "rapide_mois[1]" : $scope.user1.month-1,
                 "rapide_annee[0]" : $scope.user0.year,
                 "rapide_annee[1]" : $scope.user1.year,
+                "date1" : $scope.date1,
                 partenaire: 9999,
                 lang: "en"
             };
