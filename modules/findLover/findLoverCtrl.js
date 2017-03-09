dc9513bc7b9a9ae955a9def700913d30b0087d54
Long <zzsushiboyzz@gmail.com>
2017-03-09 23:55:22 +0700

    sua loi year

--- modules/findLover/findLoverCtrl.js
+++ modules/findLover/findLoverCtrl.js
@@ -57,7 +57,7 @@ angular.module('findLoverModule')
 
             $http({
 
-                url: 'https://cors-anywhere.herokuapp.com/http://www.astrotheme.fr/partenaires/indice_rapide.php',
+                url: 'https://thawing-lake-86928.herokuapp.com/http://www.astrotheme.fr/partenaires/indice_rapide.php',
                 method: 'POST',
                 data: $httpParamSerializerJQLike(request), // Make sure to inject the service you choose to the controller
                 headers: {
